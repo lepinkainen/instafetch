edbe5b693855fc0207150bcbeda96090788b61f1
Riku Lindblad <riku.lindblad@gmail.com>
2017-11-22 13:46:04 +0200

    Add some headers for web client

--- worker/webworker.go
+++ worker/webworker.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"errors"
+
 	log "github.com/Sirupsen/logrus"
 
 	"io/ioutil"
@@ -9,8 +11,19 @@ import (
 
 func GetPage(url string) ([]byte, error) {
 
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Errorf("Error generating new request: %v", err)
+	}
+
+	// At least make a decent attempt at faking a real browser
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-us")
+
 	// Fetch the page
-	res, err := http.Get(url)
+	res, err := client.Do(req)
 	if err != nil {
 		log.Errorln("HTTP Error", err.Error())
 		return nil, err
@@ -22,6 +35,10 @@ func GetPage(url string) ([]byte, error) {
 	if err != nil {
 		log.Errorln("Error reading response from I/O", err.Error())
 		return nil, err
+	}
+
+	if body[0] == '<' {
+		return body, errors.New("Received HTML content")
 	}
 
 	return body, nil
